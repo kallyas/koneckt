042a4b3cb913ae851be2169cedb11918f5e0a601
Kally <kallyasmedia@gmail.com>
2020-08-23 15:55:48 +0000

    update db config

--- src/db_config.php
+++ src/db_config.php
@@ -3,6 +3,6 @@
 $servername = "localhost";
 $username = "jacaresi_koneckt ";
 $password = "password";
-$db_name = "jacaresi_koneckt ";
+$db_name = "jacaresi_koneckt";
 
 $conn = new mysqli($servername, $username, $password, $db_name);
